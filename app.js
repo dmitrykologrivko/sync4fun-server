5bbb53b991aa545d67403979dd3957f82b2c27e6
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2018-05-04 18:40:37 +0300

    Draft socket events

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
-const app = express();
-const server = http.Server(app);
-const io = socketIo(server);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
     res.send('It works!');
 });
 
 io.on('connection', (socket) => {
-    console.log('Connected to WS');
+    require('./events')(socket);
 });
 
 server.listen(process.env.PORT || 8000, () => {
---
+++ events.js
@@ -0,0 +1,26 @@
+module.exports = (socket) => {
+
+    socket.on('user joined room', (req) => {
+
+    });
+
+    socket.on('user left room', (req) => {
+
+    });
+
+    socket.on('user updated file information', (req) => {
+
+    });
+
+    socket.on('user changed play state to play', (req) => {
+
+    });
+
+    socket.on('user changed play state to pause', (req) => {
+
+    });
+
+    socket.on('user changed play state to stop', (req) => {
+
+    });
+};
