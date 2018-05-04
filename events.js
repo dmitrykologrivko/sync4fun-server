ab388def6c98eee6267912a0bee16da6fa9cbf04
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2018-05-04 20:29:02 +0300

    Refactor name of events

--- events.js
+++ events.js
@@ -1,26 +1,26 @@
 module.exports = (socket) => {
 
-    socket.on('user joined room', (req) => {
+    socket.on('user join room', (req) => {
 
     });
 
-    socket.on('user left room', (req) => {
+    socket.on('user leave room', (req) => {
 
     });
 
-    socket.on('user updated file information', (req) => {
+    socket.on('user updates file information', (req) => {
 
     });
 
-    socket.on('user changed play state to play', (req) => {
+    socket.on('user change play state to play', (req) => {
 
     });
 
-    socket.on('user changed play state to pause', (req) => {
+    socket.on('user change play state to pause', (req) => {
 
     });
 
-    socket.on('user changed play state to stop', (req) => {
+    socket.on('user change play state to stop', (req) => {
 
     });
 };
